9ee9d75d64e69c7724272d9a2327c87b02e829b0
rosine <tuyishimirer1@gmail.com>
2025-11-17 19:03:55 +0200

    WEB-15: Add /home route with homepage welcome message

--- app.js
+++ app.js
@@ -1,6 +1,3 @@
-app.get('/home', (req, res) => {
-  res.send("Welcome to the Homepage!");
-});
 const express = require("express");
 const app = express();
 
@@ -8,6 +5,11 @@ app.get("/", (req, res) => {
   res.send("Hello from the Jira Integration Demo!");
 });
 
+app.get("/home", (req, res) => {
+  res.send("Welcome to the Homepage!");
+});
+
 app.listen(3000, () => {
   console.log("Server running on port 3000");
 });
+
